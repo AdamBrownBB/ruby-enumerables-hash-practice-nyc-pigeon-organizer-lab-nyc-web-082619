d80de1ec6f8e867280e43fa71054411a139282a5
Adam Brown <adam.brown.betty@gmail.com>
2019-08-23 02:44:24 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -6,7 +6,7 @@ def nyc_pigeon_organizer(data)
   data.each do |property, hash|
     hash.each do |attribute, array|
       array.each do |name|
-        if !new_hash.has_key?(name)
+        if !new_hash.has_key?(name)  #so no duplicate names
           new_hash[name] = {}
         end
 
