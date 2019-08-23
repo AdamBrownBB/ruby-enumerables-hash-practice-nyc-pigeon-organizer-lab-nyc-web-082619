71159d9b1a9ecc61271c653eb6198ee6b2416f3c
Adam Brown <adam.brown.betty@gmail.com>
2019-08-23 02:28:44 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,15 +1,21 @@
 def nyc_pigeon_organizer(data)
-  pigeon_list = {}
-  data.each do |k, v|
-  
-    pigeon_list = {}
+  new_hash = {}
+  data.each do |property, hash|
+    hash.each do |attribute, array|
+      array.each do |name|
+        if !new_hash.has_key?(name)
+          new_hash[name] = {}
+        end
 
-  data.each do |all_attributes_hash, attribute_hash|
-    attribute_hash.each do |attribute, pigeon_array|
-      pigeon_array.each do |pigeon|
-        pigeon_list[pigeon] = {:color => [], :gender => [], :lives => []}
+        if !new_hash[name].has_key?(property)
+          new_hash[name][property] = []
+        end
+
+        if !new_hash[name][property].include?(attribute)
+          new_hash[name][property] << attribute.to_s
+        end
       end
     end
-  end  
-  
+  end
+  new_hash
 end
